9743aef240be4784f558f861d0144fb4eed6317d
Manuel Heredia <manolo.heredia@gmail.com>
2022-07-31 01:06:22 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+`mhered-test-package` is a first demo package by mhered to practice
+
+  
+
---
+++ tests/test_mhered_test_pkg.py
@@ -0,0 +1,5 @@
+from mhered_test_pkg import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
