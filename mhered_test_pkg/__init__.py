d68892712f85415ef9168ff1d02148604fb6d777
Manuel Heredia <manolo.heredia@gmail.com>
2022-08-01 20:03:01 +0200

    Passes basic tests
    - refactored `__init__.py` to move main game loop to a function
      `Rock_Paper_Scissors()`, so the program does not execute at import
    - fixed `test_mhered_test_pkg.py` to find `pyproject.toml`
    - python template added to `.gitignore` to avoid committing junk files
      from pytest

--- mhered_test_pkg/__init__.py
+++ mhered_test_pkg/__init__.py
@@ -14,53 +14,60 @@ def move_msg(Move):
     return Msg
 
 
-print("ROCK, PAPER, SCISSORS")
+def Rock_Paper_Scissors():
+    """A simple Rock Paper Scissors game"""
 
-# Init variables to keep track of the number of wins, losses, and ties.
-wins = 0
-losses = 0
-ties = 0
+    print("ROCK, PAPER, SCISSORS")
 
-# Main game loop
-while True:
-    print("%s Wins, %s Losses, %s Ties" % (wins, losses, ties))
+    # Init variables to keep track of the number of wins, losses, and ties.
+    wins = 0
+    losses = 0
+    ties = 0
 
-    # Player plays
+    # Main game loop
     while True:
-        print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
-        playerMove = input()
-        if playerMove == "q":
-            print("Bye!")
-            sys.exit()  # Quit the program.
-        if playerMove in ["r", "p", "s"]:
-            break  # Break out of the player input loop.
-        print("Type one of r, p, s, or q.")
+        print("%s Wins, %s Losses, %s Ties" % (wins, losses, ties))
 
-    # Computer plays
-    computerMoves = ["r", "p", "s"]
-    computerMove = computerMoves[random.randint(0, 2)]
+        # Player plays
+        while True:
+            print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+            playerMove = input()
+            if playerMove == "q":
+                print("Bye!")
+                sys.exit()  # Quit the program.
+            if playerMove in ["r", "p", "s"]:
+                break  # Break out of the player input loop.
+            print("Type one of r, p, s, or q.")
 
-    print(move_msg(playerMove) + " versus... " + move_msg(computerMove))
+        # Computer plays
+        computerMoves = ["r", "p", "s"]
+        computerMove = computerMoves[random.randint(0, 2)]
 
-    # Display and record the win/loss/tie:
-    if playerMove == computerMove:
-        print("It is a tie!")
-        ties = ties + 1
-    elif playerMove == "r" and computerMove == "s":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "p" and computerMove == "r":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "s" and computerMove == "p":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "r" and computerMove == "p":
-        print("You lose!")
-        losses = losses + 1
-    elif playerMove == "p" and computerMove == "s":
-        print("You lose!")
-        losses = losses + 1
-    elif playerMove == "s" and computerMove == "r":
-        print("You lose!")
-        losses = losses + 1
+        print(move_msg(playerMove) + " versus... " + move_msg(computerMove))
+
+        # Display and record the win/loss/tie:
+        if playerMove == computerMove:
+            print("It is a tie!")
+            ties = ties + 1
+        elif playerMove == "r" and computerMove == "s":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "p" and computerMove == "r":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "s" and computerMove == "p":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "r" and computerMove == "p":
+            print("You lose!")
+            losses = losses + 1
+        elif playerMove == "p" and computerMove == "s":
+            print("You lose!")
+            losses = losses + 1
+        elif playerMove == "s" and computerMove == "r":
+            print("You lose!")
+            losses = losses + 1
+
+
+if __name__ == "__main__":
+    Rock_Paper_Scissors()
--- tests/test_mhered_test_pkg.py
+++ tests/test_mhered_test_pkg.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 import toml
 
-import mhered_test_pkg
+from mhered_test_pkg import __version__
 
 
 def test_versions_are_in_sync():
     """Checks if tool.poetry.version in pyproject.toml and
     __version__ in mhered_test_pkg.__init__.py are in sync."""
 
-    path = Path(__file__).resolve().parents[2] / "pyproject.toml"
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
     pyproject = toml.loads(open(str(path)).read())
     pyproject_version = pyproject["tool"]["poetry"]["version"]
 
-    init_py_version = mhered_test_pkg.__version__
+    init_py_version = __version__
 
     assert init_py_version == pyproject_version
